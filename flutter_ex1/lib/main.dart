da3b08c09fc07fceb8612648fbc6548e2f9bc026
Pedro-Gouveia <72574592+Pedro-Gouveia@users.noreply.github.com>
2021-01-16 17:29:58 +0000

    flutter

---
+++ flutter_ex1/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Hello World')
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text ('Click Me')
+      ),
+    )
+  ));
+}
+
+
